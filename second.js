c1668643b7166769a9ef4a0becac06a8bce437c0
hridoy <hridoydasewu@gmail.com>
2026-03-07 23:34:02 +0600

    added mytest as gitignore

--- second.js
+++
@@ -1,5 +0,0 @@
-const all_data = ["Hello batch 16.", 3, 8, "text", ["arr_2_1", "arr_2_2", "arr_2_3"]];
-// const last_index = all_data.length - 1;
-// const second_array = all_data[last_index];
-// const last_index_of_second = second_array.length - 1;
-console.log(all_data[all_data.length - 1][all_data[all_data.length - 1].length - 1]);
